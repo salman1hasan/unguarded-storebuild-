f340e8e1a79b0bcfa79d7219bc5124875f9ede02
Salman Hasan [Salman Zuberi Hussain] <s.hasan@unguarded.com>
2023-11-25 17:31:09 -0500

    Add files via upload

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -6,19 +6,25 @@ import {
   Image,
   TextInput,
   KeyboardAvoidingView,
+  Pressable,
 } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigation = useNavigation;
+
   return (
     <SafeAreaView
       style={{ flex: 1, backgroundColor: 'white', alignItems: 'center' }}
     >
       <View>
         <Image
-          style={{ width: 150, height: 100 }}
+          style={{ width: 250, height: 250 }}
           source={{
             uri: 'https://assets.stickpng.com/thumbs/6160562276000b00045a7d97.png',
           }}
@@ -38,7 +44,7 @@ const LoginScreen = () => {
           </Text>
         </View>
 
-        <View style={{ marginTop: 70 }}>
+        <View>
           <View
             style={{
               flexDirection: 'row',
@@ -54,39 +60,95 @@ const LoginScreen = () => {
               style={{ marginLeft: 8 }}
               name="email"
               size={24}
-              color="black"
+              color="gray"
+            />
+            <TextInput
+              value={email}
+              onChange={(text) => setEmail(text)}
+              style={{
+                color: 'gray',
+                marginVertical: 10,
+                width: 300,
+                fontSize: email ? 18 : 18,
+              }}
+              placeholder="enter your Email"
+            />
+          </View>
+        </View>
+
+        <View style={{ marginTop: 10 }}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              gap: 5,
+              backgroundColor: '#D0D0D0',
+              paddingVertical: 5,
+              borderRadius: 5,
+              marginTop: 30,
+            }}
+          >
+            <AntDesign
+              name="lock1"
+              size={24}
+              color="gray"
+              style={{ marginLeft: 8 }}
             />
 
             <TextInput
-              style={{ color: 'gray', marginVeritcal: 10, width: 300 }}
-              placeholder="enter your Email"
+              value={password}
+              onChange={(text) => setPassword(text)}
+              secureTextEntry={true}
+              style={{
+                color: 'gray',
+                marginVertical: 10,
+                width: 300,
+                fontSize: password ? 18 : 18,
+              }}
+              placeholder="enter your password"
             />
           </View>
         </View>
-        <View style={{ marginTop: 0 }}>
-          <View
+        <View
+          style={{
+            marginTop: 12,
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Text>Keep me logged in</Text>
+          <Text style={{ color: '#007FFF', fontWeight: '500' }}>
+            Forgot Password
+          </Text>
+        </View>
+        <View style={{ marginTop: 50 }} />
+        <Pressable
+          style={{
+            width: 200,
+            backgroundColor: '#FEBE10',
+            borderRadius: 6,
+            marginLeft: 'auto',
+            marginRight: 'auto',
+            padding: 15,
+          }}
+        >
+          <Text
             style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              gap: 5,
-              backgroundColor: '#D0D0D0',
-              paddingVertical: 5,
-              borderRadius: 5,
-              marginTop: 30,
+              textAlign: 'center',
+              color: 'white',
+              fontSize: 16,
+              fontWeight: 'bold',
             }}
           >
-            <MaterialIcons
-              style={{ marginLeft: 8 }}
-              name="email"
-              size={24}
-              color="black"
-            />
-            <TextInput
-              style={{ color: 'gray', marginVeritcal: 10, width: 300 }}
-              placeholder="enter your Email"
-            />
-          </View>
-        </View>
+            Login
+          </Text>
+        </Pressable>
+        <Pressable style={{ marginTop: 15 }}>
+          <Text style={{ textAlign: 'center', color: 'gray', fontSize: 16 }}>
+            Don't have an account? Sign Up
+          </Text>
+        </Pressable>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
